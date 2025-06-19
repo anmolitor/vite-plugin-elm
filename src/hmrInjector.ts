f77867e075f6d16b0c72d913fe285a8e4e1c711b
amolitor <andreas.molitor@andrena.de>
2025-06-20 00:47:32 +0200

    Fix issue in hmrInjector resulting in huge warnings with vite 6

--- src/hmrInjector.ts
+++ src/hmrInjector.ts
@@ -45,7 +45,7 @@ if (import.meta.hot) {
 
   import.meta.hot.accept()
   import.meta.hot.accept([
-    "${dependencies.join('", "')}"
+    ${dependencies.join('", "')}
   ], () => { console.log("[vite-plugin-elm] Dependency is updated") })
 
   import.meta.hot.on('hot-update-dependents', (data) => {
